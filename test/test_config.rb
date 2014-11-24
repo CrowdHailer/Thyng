763cc137367012ca4dc3e9b707adc4906ec15729
Peter <peterhsaxton@gmail.com>
2014-11-24 14:05:32 +0000

    color test output

--- test/test_config.rb
+++ test/test_config.rb
@@ -1,2 +1,6 @@
 require 'thyng'
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+reporter_options = {color: true}
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
