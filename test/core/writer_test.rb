58bf31152b3d7727b913bae2d69bf00e612d0bed
Peter <peterhsaxton@gmail.com>
2014-11-24 11:25:13 +0000

    fix tests to fail within test block

--- test/core/writer_test.rb
+++ test/core/writer_test.rb
@@ -1,22 +1,24 @@
 require_relative '../test_config'
 
-class WriterExample < Thyng
-  aspect_writer :name
-end
-
 class WriterTest < MiniTest::Test
   def gomez
     'Gomez'
   end
 
+  def writer_example
+    Class.new Thyng do
+      aspect_writer :name
+    end
+  end
+
   def test_can_set_aspect
-    example = WriterExample.new
+    example = writer_example.new
     example.name = gomez
     assert_equal gomez, example[:name]
   end
 
   def test_responds_to_writer_method
-    example = WriterExample.new
+    example = writer_example.new
     assert example.respond_to?('name='), 'Thyng should report response to writers'
   end
 end
