abf521886e6f373b280255828a0a2b2e518f9189
Peter <peterhsaxton@gmail.com>
2014-11-24 14:06:43 +0000

    show slow tests

--- test/test_config.rb
+++ test/test_config.rb
@@ -2,5 +2,5 @@ require 'thyng'
 require 'minitest/autorun'
 require 'minitest/reporters'
 
-reporter_options = {color: true}
+reporter_options = {color: true, slow_count: 5}
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
